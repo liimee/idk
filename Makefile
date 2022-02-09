10de818517f00422887052b903cb62599eb28d69
ta <alt3753.7@gmail.com>
2022-02-09 18:54:00 +0700

    e

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
-build: main.go t/
+build: build_fe build_be
+
+build_fe: t/
 	yarn build
+
+build_be: main.go
 	go build
+
+dev: build_be
+	yarn dev & DEV=y ./idk
--- main.go
+++ main.go
@@ -5,22 +5,31 @@ import (
 	"fmt"
 	"io/fs"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 //go:embed build/*
 var f embed.FS
+var dev = os.Getenv("DEV") == "y"
 
 func main() {
 	e := mux.NewRouter()
 
-	e.PathPrefix("/").HandlerFunc(fe)
+	if !dev {
+		e.PathPrefix("/").HandlerFunc(fe)
+	}
+
+	s := ":3000"
+	if dev {
+		s = ":4000"
+	}
 
 	fmt.Println("running")
 
 	http.Handle("/", e)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(s, nil)
 }
 
 func fe(w http.ResponseWriter, r *http.Request) {
