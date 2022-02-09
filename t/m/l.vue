50c4cb97849988b8f786a56606cbe0d23ceff16e
ta <alt3753.7@gmail.com>
2022-02-09 16:23:09 +0700

    oops

---
+++ t/m/l.vue
@@ -0,0 +1,48 @@
+<template>
+<h1>Hmm</h1>
+<div class="s">
+  <button>New Game</button>
+  <button>Join Game</button>
+</div>
+</template>
+
+<style scoped>
+button:first-child {
+  background-color: #D67021;
+  box-shadow: 0 4px #ac5a1c;
+}
+
+button:last-child {
+  background-color: #2C6E49;
+  box-shadow: 0 4px #235739;
+}
+
+button {
+  border-radius: 11px;
+  padding: 1em 3em;
+  width: 25vw;
+  font-size: 1.2em;
+  font-weight: bold;
+  border: none;
+  color: #fff;
+  margin: 2em;
+  min-width: 200px;
+}
+
+button:active {
+  box-shadow: none;
+  transform: translateY(4px);
+}
+
+.s {
+  text-align: center;
+}
+
+@media (max-width: 290px) {
+  button {
+    margin: 2em .5em;
+    width: calc(100vw - 2em);
+    min-width: 0;
+  }
+}
+</style>
---
+++ t/main.ts
@@ -0,0 +1,4 @@
+import {createApp} from 'vue';
+import App from './m/l.vue';
+
+createApp(App).mount('#app')
