2220961d1adbc02e8493cf72fa7019b1dbe75d4a
ta <alt3753.7@gmail.com>
2022-02-10 07:25:56 +0700

    style

--- t/m/l.vue
+++ t/m/l.vue
@@ -7,33 +7,6 @@
 </template>
 
 <style scoped>
-.a:first-child {
-  background-color: #D67021;
-  box-shadow: 0 4px #ac5a1c;
-}
-
-.b:last-child {
-  background-color: #2C6E49;
-  box-shadow: 0 4px #235739;
-}
-
-button {
-  border-radius: 11px;
-  padding: 1em 3em;
-  width: 25vw;
-  font-size: 1.2em;
-  font-weight: bold;
-  border: none;
-  color: #fff;
-  margin: 2em;
-  min-width: 200px;
-}
-
-button:active {
-  box-shadow: none !important;
-  transform: translateY(4px);
-}
-
 .s {
   text-align: center;
 }
--- t/m/n.vue
+++ t/m/n.vue
@@ -1,23 +1,55 @@
 <template>
-  Waiting for server...
+  <form @submit="start">
+    <label for="joinas">Join as...</label>
+    <input type="text" placeholder="Hm?" id="joinas" v-model="as" required>
+    <input type="submit" class="a" value="Join">
+  </form>
 </template>
+
+<style scoped>
+form {
+  text-align: center;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  font-size: 1.1em;
+}
+
+input[type="text"] {
+  padding: .4em;
+  border-radius: 6px;
+  border: solid 2px #000;
+  font-size: 1.3em;
+}
+
+label, input {
+  margin: .3em;
+  vertical-align: middle;
+}
+</style>
 
 <script lang="ts">
 export default {
   data() {
     return {
-      err: false
+      err: false,
+      as: ''
     }
   },
-  mounted() {
-    fetch((import.meta.env.VITE_API||'')+'/api/new', {
-      method: 'POST'
-    }).then(v => {
-      if(v.ok) return v.json();
-      this.err = true;
-    }).then(v => {
-      this.$router.replace("/g/"+v.id)
-    })
+  methods: {
+    start(e: Event) {
+      e.preventDefault();
+      fetch((import.meta.env.VITE_API||'')+'/api/new', {
+        method: 'POST'
+      }).then(v => {
+        if(v.ok) return v.json();
+        this.err = true;
+      }).then(v => {
+        this.$router.replace("/g/"+v.id)
+      })
+    }
   }
 }
 </script>
